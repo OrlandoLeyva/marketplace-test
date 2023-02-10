4aa1b156248e312a30fe8de50e7339488f8898ff
orlando leyva <leyvaorlandomx08@gmail.com>
2023-02-10 14:38:54 -0700

    Basic auth system: register, login and logout

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'market'
+    'market',
+    'user_auth',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
--- marketplace/urls.py
+++ marketplace/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('market/', include('market.urls')),
+    path('account/', include('user_auth.urls')),
     path('api-auth/', include('rest_framework.urls'))
 ]
 
